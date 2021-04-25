252856f6910ae6290e89f939bfb6ede73bd66c08
Dalzon Charles-Hébert <charlby5@gmail.com>
2021-04-25 16:18:52 -0400

    Show all the posts

---
+++ client/src/PostList.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const PostList = () => {
+  const [posts, setPosts] = useState({});
+
+  const fetchPosts = async () => {
+    const { data } = await axios.get('http://localhost:4000/posts');
+    setPosts(data);
+  };
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  const renderedPosts = Object.values(posts).map((post) => {
+    return (
+      <div
+        className='card'
+        style={{ width: '30%', marginBlock: '20px' }}
+        key={post.id}
+      >
+        <div className='card-body'>
+          <h3>{post.title}</h3>
+        </div>
+      </div>
+    );
+  });
+
+  return (
+    <div className='d-flex flex-row flex-wrap justify-content-between'>
+      {renderedPosts}
+    </div>
+  );
+};
+
+export default PostList;
