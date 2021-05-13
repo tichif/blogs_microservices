ab6a10dc9f0a4bc547ec0317f91b68abb8b1832e
Dalzon Charles-Hébert <charlby5@gmail.com>
2021-05-12 22:28:37 -0400

    Render comment based on the status

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -2,10 +2,18 @@ import React from 'react';
 
 const CommentList = ({ comments }) => {
   let renderedComments;
+  let content;
   if (comments) {
-    renderedComments = comments.map((comment) => (
-      <li key={comment.id}>{comment.content}</li>
-    ));
+    renderedComments = comments.map((comment) => {
+      if (comment.status === 'approved') {
+        content = comment.content;
+      } else if (comment.status === 'pending') {
+        content = 'This comment is awaiting moderation';
+      } else if (comment.status === 'reject') {
+        content = 'This comment has been rejected.';
+      }
+      return <li key={comment.id}>{content}</li>;
+    });
   }
 
   return <ul>{renderedComments}</ul>;
